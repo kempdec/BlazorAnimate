75a42b388720e69833b49474a7ad9bd4271eb0c5
Vinícius Lima <viniciusxdl@outlook.com>
2023-11-18 18:47:27 -0300

    Adicionar os modos de preenchimento.

---
+++ src/BlazorApp.Animate/FillModes/ForwardsFillMode.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.Animate.FillModes;
+
+/// <summary>
+/// Representa o modo de preenchimento "forwards" que define que o elemento manterá os estilos finais da animação após
+/// a sua conclusão.
+/// </summary>
+public sealed class ForwardsFillMode : IFillMode
+{
+    public string Value { get; } = "forwards";
+}
---
+++ src/BlazorApp.Animate/FillModes/NoneFillMode.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.Animate.FillModes;
+
+/// <summary>
+/// Representa o modo de preenchimento "none" que define que o elemento não terá estilos aplicados antes ou depois da
+/// animação. Os estilos são aplicados apenas durante a execução da animação.
+/// </summary>
+public sealed class NoneFillMode : IFillMode
+{
+    public string Value { get; } = "none";
+}
